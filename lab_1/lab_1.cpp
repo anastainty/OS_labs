7af5cdce46160a500a76d431b896b2d785feef77
Anastasia Intyakova <116066268+anastainty@users.noreply.github.com>
2023-10-17 21:16:16 +0300

    Lab 1 Part 1

---
+++ lab_1/lab_1.cpp
@@ -0,0 +1,17 @@
+﻿#include <windows.h>
+#include <iostream>
+
+void OnSignal(int signal) {
+    std::cout << "Signal received: " << signal << std::endl;
+    exit(0);
+}
+
+int main() {
+    SetConsoleCtrlHandler((PHANDLER_ROUTINE)OnSignal, TRUE);
+
+    while (true) {
+        Sleep(1000);
+    }
+
+    return 0;
+}
